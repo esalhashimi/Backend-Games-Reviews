08924fa93badb98aefa10181bcc4cb9bfac23c34
esaalhashimi <esahh222@gmail.com>
2026-01-25 14:47:37 +0300

    Edit Path Image

--- data/game_data.py
+++ data/game_data.py
@@ -4,8 +4,8 @@ from models.game import GameModel
 games_list = [
     GameModel(name="The Legend of Zelda™: Tears of the Kingdom",
                gender="Action Adventure",
-               image="https://static.wikia.nocookie.net/nintendo/images/a/a8/The_Legend_of_Zelda_Tears_of_the_Kingdom_%28NA%29.jpg/revision/latest?cb=20230213034219&path-prefix=en",
-               description="In this sequel to the Legend of Zelda: Breath of the Wild game, you’ll decide your own path through the sprawling landscapes of Hyrule and the mysterious islands floating in the vast skies above. Can you harness the power of Link’s abilities to fight back against the malevolent forces that threaten the kingdom?",
+               image="https://imgs.search.brave.com/Gp4kK8QQOjuELq25AePrdgEdmDYshHrrFoHVLeegpEo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMubGF1bmNoYm94/LWFwcC5jb20vZWQ0/NWVlOTYtNDhlZS00/NmFiLTgxOGEtMWVi/YzFmNDVhOGYxLmpw/Zw",
+               description="In this sequel to the Legend of Zelda: Breath of the Wild game, you'll decide your own path through the sprawling landscapes of Hyrule and the mysterious islands floating in the vast skies above. Can you harness the power of Link’s abilities to fight back against the malevolent forces that threaten the kingdom?",
                publisher="Nintendo",
                rate= 10,
                  is_released=True, user_id=1)
