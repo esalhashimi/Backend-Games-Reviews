99c2ffec21c5ceea7f36f87cc9f1027ab2855710
esaalhashimi <esahh222@gmail.com>
2026-01-25 16:12:24 +0300

    Edit controller user

--- controllers/users.py
+++ controllers/users.py
@@ -6,38 +6,37 @@ from database import get_db
 
 router = APIRouter()
 
-@router.post("/register", response_model=UserSchema)
+@router.post("/register", response_model=UserTokenSchema) 
 def create_user(user: UserRegistrationSchema, db: Session = Depends(get_db)):
-    # Check if the username or email already exists
+    
     existing_user = db.query(UserModel).filter(
         (UserModel.username == user.username) | (UserModel.email == user.email)
     ).first()
 
     if existing_user:
+
         raise HTTPException(status_code=409, detail="Username or email already exists")
 
     new_user = UserModel(username=user.username, email=user.email)
-    # Use the set_password method to hash the password
     new_user.set_password(user.password)
 
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
-    return new_user
+    token = new_user.generate_token()
+
+    return {
+        "token": token, 
+        "message": "User created and logged in successfully"
+    }
 
 @router.post("/login", response_model=UserTokenSchema)
 def login(user: UserLoginSchema, db: Session = Depends(get_db)):
-
-    # Find the user by username
     db_user = db.query(UserModel).filter(UserModel.username == user.username).first()
 
-    # Check if the user exists and if the password is correct
     if not db_user or not db_user.verify_password(user.password):
         raise HTTPException(status_code=400, detail="Invalid username or password")
 
-    # Generate JWT token
     token = db_user.generate_token()
-
-    # Return token and a success message
     return {"token": token, "message": "Login successful"}
